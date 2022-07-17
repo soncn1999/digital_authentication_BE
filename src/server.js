5f3f22191f5be59c731a7e836531ff78c570120c
soncn1999 <shasacaoson1999@gmail.com>
2022-07-17 09:11:28 +0700

    Render to .ejs

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
 require('dotenv').config();
 
+import connectDB from './config/connectDB';
+
 let app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
